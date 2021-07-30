870544d21356e119d9be3c3fa7bbb26292d91c2b
JanaPrikrylova <prikrylovajana003@gmail.com>
2021-07-30 18:39:48 +0200

    Sekce Zvířata dle druhu

--- index.js
+++ index.js
@@ -35,3 +35,51 @@ imgElm[2].addEventListener("click", () => {
   homeNameElm.textContent = "Tyger";
 })
 
+// ----------------------------------------------------
+
+let span = document.getElementsByClassName('species__span');
+let product = document.getElementsByClassName('species__item');
+let product_page = Math.ceil(product.length/4);
+let l = 0;
+let movePer = 25.34;
+let maxMove = 60;
+
+// Mobile_view	
+
+let mob_view = window.matchMedia("(max-width: 768px)");
+if (mob_view.matches)
+ {
+  movePer = 53;
+  maxMove = 225;
+ }
+
+
+ let mob_view1 = window.matchMedia("(max-width: 360px)");
+ if (mob_view1.matches)
+
+  {
+    movePer = 105;
+    maxMove = 600;
+  }
+
+let right_mover = ()=>{
+  l = l + movePer;
+  if (product == 1){l = 0; }
+  for(const i of product)
+  {
+    if (l > maxMove){l = l - movePer;}
+    i.style.left = '-' + l + '%';
+  }
+
+}
+let left_mover = ()=>{
+  l = l - movePer;
+  if (l<=0){l = 0;}
+  for(const i of product){
+    if (product_page>1){
+      i.style.left = '-' + l + '%';
+    }
+  }
+}
+span[1].onclick = ()=>{right_mover();}
+span[0].onclick = ()=>{left_mover();}
